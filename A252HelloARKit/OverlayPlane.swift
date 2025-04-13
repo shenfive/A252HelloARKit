74575bd48e3a9aeacaec1702e5d6e0558fc5dfac
Danny Shen <shenfive@gmail.com>
2025-04-13 17:20:19 +0800

    找到 Plant

---
+++ A252HelloARKit/OverlayPlane.swift
@@ -0,0 +1,42 @@
+//
+//  OverlayPlane.swift
+//  A252HelloARKit
+//
+//  Created by 申潤五 on 2025/4/13.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class OverlayPlane: SCNNode {
+    var anchor :ARPlaneAnchor
+    var planeGeometry :SCNPlane!
+    
+    init(anchor :ARPlaneAnchor) {
+        self.anchor = anchor
+        super.init()
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setup() {
+        self.planeGeometry = SCNPlane(width: CGFloat(self.anchor.extent.x),
+                                      height: CGFloat(self.anchor.extent.z))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named:"overlay_grid.png")
+        self.planeGeometry.materials = [material]
+        let planeNode = SCNNode(geometry: self.planeGeometry)
+        planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z);
+        planeNode.transform = SCNMatrix4MakeRotation(Float(-Double.pi / 2.0), 1, 0.0, 0.0)
+        //向（1,0,0) 座標轉 二分之π 即 90°
+        planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: self.planeGeometry, options: nil))
+        self.addChildNode(planeNode)
+    }
+    
+    
+}
